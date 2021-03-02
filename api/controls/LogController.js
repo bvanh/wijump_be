383a8d85b383888d93c77d7da327728049052390
vietahn <bv.anh194@gmail.com>
2021-03-03 00:14:30 +0700

    f

--- api/controls/LogController.js
+++ api/controls/LogController.js
@@ -4,20 +4,40 @@ const sequelize = require("sequelize");
 const attributesCharges = ["data"];
 module.exports = {
   saveConfig: async (req, res, next) => {
-    const { id, config } = req.body;
+    const { config } = req.body;
     Config.update(
       {
         data: config,
       },
       {
         where: {
-          map_id: id,
+          map_id: 1,
         },
       }
     )
       .then((data) => {
         res.status(200).send({
           message: "Cập nhật thành công",
+        });
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving tutorials.",
+        });
+      });
+  },
+  getConfig: async (req, res, next) => {
+    Config.findAll({
+      attributes: ["data"],
+      where: {
+        map_id: 1,
+      },
+    })
+      .then((data) => {
+        res.status(200).send({
+          message: "get thành công",
+          data: data,
         });
       })
       .catch((err) => {
